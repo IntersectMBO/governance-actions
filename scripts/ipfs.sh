68d2cd7cebef029db2d0c4ef72cad0df4705c4f2
Ryan Williams <ryan.williams@intersectmbo.org>
2025-06-20 00:19:07 +0100

    add pinata and clean up

--- scripts/ipfs.sh
+++ scripts/ipfs.sh
@@ -16,6 +16,7 @@ DEFAULT_GATEWAY_2="https://gateway.pinata.cloud/ipfs/"
 DEFAULT_HOST_ON_LOCAL_NODE="true"
 DEFAULT_HOST_ON_NMKR="true"
 DEFAULT_HOST_ON_BLOCKFROST="true"
+DEFAULT_HOST_ON_PINATA="true"
 
 # Pinata is $20 a month to support API
 # HOST_ON_PINATA="true"
@@ -33,7 +34,7 @@ fi
 
 # Usage message
 usage() {
-    echo "Usage: $0 <file> [--just-pin] [--just-check] [--no-local] [--no-blockfrost] [--no-nmkr]"
+    echo "Usage: $0 <file> [--just-pin] [--just-check] [--no-local] [--no-pinata] [--no-blockfrost] [--no-nmkr]"
     echo "Check if a file is on IPFS, and also pin it locally and via Blockfrost and NMKR."
     echo "  "
     echo "Options:"
@@ -41,6 +42,7 @@ usage() {
     echo "  --just-pin              Don't look for the file, just pin it (default: $JUST_PIN)"
     echo "  --just-check            Only look for the file don't try to pin it (default: $JUST_CHECK)"
     echo "  --no-local              Don't try to pin file on local ipfs node? (default: $DEFAULT_HOST_ON_LOCAL_NODE)"
+    echo "  --no-pinata             Don't try to pin file on pinata service? (default: $DEFAULT_HOST_ON_PINATA)"
     echo "  --no-blockfrost         Don't try to pin file on blockfrost service? (default: $DEFAULT_HOST_ON_BLOCKFROST)"
     echo "  --no-nmkr               Don't try to pin file on NMKR service? (default: $DEFAULT_HOST_ON_NMKR_STORAGE)"
     exit 1
@@ -51,6 +53,7 @@ input_path=""
 just_pin="$JUST_PIN"
 just_check="$JUST_CHECK"
 local_host="$DEFAULT_HOST_ON_LOCAL_NODE"
+pinata_host="$DEFAULT_HOST_ON_PINATA"
 blockfrost_host="$DEFAULT_HOST_ON_BLOCKFROST"
 nmkr_host="$DEFAULT_HOST_ON_NMKR"
 
@@ -67,6 +70,10 @@ while [[ $# -gt 0 ]]; do
             ;;
         --no-local)
             local_host="false"
+            shift
+            ;;
+        --no-pinata)
+            pinata_host="false"
             shift
             ;;
         --no-blockfrost)
@@ -155,7 +162,38 @@ fi
 
 # Pin on local node's remote services
 # todo
-local_node_pinning_services=$(cat ipfs pin remote service ls)
+local_node_pinning_services=$(ipfs pin remote service ls)
+
+# Pin on Pinata
+echo " "
+echo "Pinning file to Pinata..."
+
+if [ "$pinata_host" = "true" ]; then
+    # Check for secret environment variables
+    # todo, this in a nicer way
+    echo "Reading Pinata API key from environment variable..."
+    if [ -z "$PINATA_API_KEY" ]; then
+        echo "Error: PINATA_API_KEY environment variable is not set." >&2
+        exit 1
+    fi
+    
+    echo "Uploading file to Pinata service..."
+    response=$(curl -s -X POST "https://uploads.pinata.cloud/v3/files" \
+                -H "Authorization: Bearer ${PINATA_API_KEY}" \
+                -F "file=@$input_path" \
+                -F "network=public" \
+            )
+    # Check response for errors
+    if echo "$response" | grep -q '"errors":'; then
+        echo "Error in Pinata response:" >&2
+        echo "$response" | jq . >&2
+        exit 1
+    fi
+
+    echo "Pinata upload successful!"
+else
+    echo "Skipping pinning on Pinata."
+fi
 
 # Pin on Blockfrost
 echo " "
@@ -171,8 +209,7 @@ if [ "$blockfrost_host" = "true" ]; then
     fi
     
     echo "Uploading file to Blockfrost service..."
-    response=$(curl "https://ipfs.blockfrost.io/api/v0/ipfs/add" \
-                -X POST \
+    response=$(curl -s -X POST "https://ipfs.blockfrost.io/api/v0/ipfs/add" \
                 -H "project_id: $BLOCKFROST_API_KEY" \
                 -F "file=@$input_path" \
             )
@@ -212,7 +249,7 @@ if [ "$nmkr_host" = "true" ]; then
 
 
     echo "Uploading file to NMKR service..."
-    response=$(curl -X POST "https://studio-api.nmkr.io/v2/UploadToIpfs/${NMKR_USER_ID}" \
+    response=$(curl -s -X POST "https://studio-api.nmkr.io/v2/UploadToIpfs/${NMKR_USER_ID}" \
         -H 'accept: text/plain' \
         -H 'Content-Type: application/json' \
         -H "Authorization: Bearer ${NMKR_API_KEY}" \
