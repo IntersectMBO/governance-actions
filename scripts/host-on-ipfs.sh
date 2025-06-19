3299bd67267e1081db4c884ed354b67055505f3c
Ryan Williams <ryan.williams@intersectmbo.org>
2025-06-19 18:41:15 +0100

    init host on IPFS script

---
+++ scripts/host-on-ipfs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+######################################################
+
+# Can change if you please
+
+# Gateways to check if file is already hosted on IPFS
+DEFAULT_GATEWAY_1="https://ipfs.io/ipfs/"
+DEFAULT_GATEWAY_2="https://gateway.pinata.cloud/ipfs/"
+
+# Gateways to host the file on IPFS
+HOST_ON_LOCAL_NODE="true"
+HOST_ON_PINATA="true"
+HOST_ON_BLOCKFROST="true"
+HOST_ON_WEB3_STORAGE="true"
+######################################################
+
+# Generate CID from the given file
+
+# check two gateways if file can be accessed
+
+# if file is not accessible then host on IPFS
+
