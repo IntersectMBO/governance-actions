b14ebf25025bd87a824840f658dce5e0ad61690a
Ryan Williams <ryan.williams@intersectmbo.org>
2025-06-19 19:26:53 +0100

    add usage message

--- scripts/host-on-ipfs.sh
+++ scripts/host-on-ipfs.sh
@@ -15,9 +15,32 @@ HOST_ON_BLOCKFROST="true"
 HOST_ON_WEB3_STORAGE="true"
 ######################################################
 
+# check if user has ipfs cli installed
+if ! command -v ipfs >/dev/null 2>&1; then
+  echo "Error: ipfs cli is not installed or not in your PATH." >&2
+  exit 1
+fi
+
+# Usage message
+usage() {
+    echo "Usage: $0 <jsonld-file> [--pin-local] [--pin-pinata] [--pin-blockfrost] [--pin-web3-storage]"
+    echo "Options:"
+    echo "  --pin-local             Try to pin file on local ipfs node? (default: $HOST_ON_LOCAL_NODE)"
+    echo "  --pin-pinata            Try to pin file on pinata service? (default: $HOST_ON_PINATA)"
+    echo "  --pin-blockfrost        Try to pin file on blockfrost service? (default: $HOST_ON_BLOCKFROST)"
+    echo "  --pin-web3-storage      Try to pin file on web3.storage service? (default: $HOST_ON_WEB3_STORAGE)"
+    exit 1
+}
+
+# Check correct number of arguments
+if [ "$#" -lt 1 ]; then
+    usage
+fi
+
 # Generate CID from the given file
+echo "Generating CID for the file..."
 
 # check two gateways if file can be accessed
 
-# if file is not accessible then host on IPFS
 
+# if file is not accessible then host on IPFS
